bd5688fa63a5b02d7c77410957fbb7d1725f0c08
zhanglwtec <zhanglw@walkingtec.cn>
2017-07-23 18:07:04 +0800

    创建常量文件

---
+++ src/Yixin/Taoche/Util/Constant.java
@@ -0,0 +1,8 @@
+package Yixin.Taoche.Util;
+
+public class Constant {
+	// 定义用户名常量
+	public static final String UserName = "15911056310";
+	// 定义密码常量
+	public static final String Password = "mgl_19890303";
+}
